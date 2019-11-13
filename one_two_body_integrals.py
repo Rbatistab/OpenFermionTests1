405450de64cdf611de08b542d99d260c8767d2c4
Rbatistab <russbatisbarrrco@gmail.com>
2019-11-13 08:09:57 -0600

    One and two body integrals

---
+++ one_two_body_integrals.py
@@ -0,0 +1,25 @@
+from openfermion.hamiltonians import MolecularData
+from openfermionpsi4 import run_psi4
+
+geometry = [['H', [0,0,0] ],
+            ['H', [0,0,0.74] ]]
+
+basis = 'sto-3g'
+multiplicity = 1
+charge = 0
+h2_molecule = MolecularData(geometry, basis, multiplicity, charge)
+
+h2_molecule = run_psi4( h2_molecule,
+                        run_mp2=True,
+                        run_cisd=True,
+                        run_ccsd=True,
+                        run_fci=True)
+
+two_electron_integrals = h2_molecule.two_body_integrals
+one_electron_integrals = h2_molecule.one_body_integrals
+
+print("Two electron integrals:")
+print(two_electron_integrals)
+print("One electron integrals")
+print(one_electron_integrals)
+

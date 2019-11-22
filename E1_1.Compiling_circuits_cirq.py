65b131a618ed02b9402f02e93370bb2a4e50d474
Rbatistab <russbatisbarrrco@gmail.com>
2019-11-22 13:14:44 -0600

    Still on forest

--- E1_1.Compiling_circuits_cirq.py
+++ E1_1.Compiling_circuits_cirq.py
@@ -38,7 +38,7 @@ import openfermioncirq as ofc
 
 hamiltonian = h2_molecule.get_molecular_hamiltonian()
 qubits = cirq.LineQubit.range(4)
-circuit = cirq.Circuit.from_ops(
+circuit = cirq.Circuit(         # change by deprications: cirq.Circuit_from ops ---> cirq.Circuit, reasons bellow 
         ofc.simulate_trotter(
             qubits,
             hamiltonian,
@@ -49,7 +49,19 @@ circuit = cirq.Circuit.from_ops(
             omit_final_swaps = True
         )
     )
+# WARNING:root:DEPRECATION
+# The function Circuit.from_ops was used but is deprecated.
+# It will be removed in cirq v0.8.0.
+# use `cirq.Circuit(*ops)` instead.
 
 cirq.merge_single_qubit_gates_into_phased_x_z(circuit)
 print("Evolution of molecular Hamiltonian for H2")
-print(circuit.to_text_diagram(use_unicode_characters=False))
+output_string = circuit.to_text_diagram(use_unicode_characters=False)
+print(output_string)
+
+# output file generation for further and more contable use
+output_file = open("E1_1.Compiling_circuits_cirq_OUTPUT.txt", "w")
+output_file.write(output_string)
+output_file.close()
+
+

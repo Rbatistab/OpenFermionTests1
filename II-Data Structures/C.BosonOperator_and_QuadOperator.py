7ec04d42f249d1c4204aac8b3e2e1c5d035a9eeb
rbatistab <russbatisbarrrco@gmail.com>
2020-01-09 08:13:12 -0600

    More on data structures

--- II-Data Structures/C.BosonOperator_and_QuadOperator.py
+++ II-Data Structures/C.BosonOperator_and_QuadOperator.py
@@ -1,3 +1,26 @@
 # C. BosonOperator and QuadOperator
 
+from openfermion.ops import BosonOperator, QuadOperator
 
+print("Bosonic Operator b^3b5b^1b4, note ordering:")
+O = BosonOperator('3^ 5 1^ 4')
+print(O)
+
+print("\nNormal ordering of bosonic operator b0b^0 = I + b^0b0:")
+from openfermion.utils import normal_ordered
+NOP = normal_ordered(BosonOperator('0 0^'))
+print(NOP)
+
+
+print("\nQuadrature Operators: \nQO = ")
+QO = QuadOperator('q0 p1 q3')
+print(QO)
+QON = normal_ordered(QuadOperator('p0 q0'), hbar = 2)
+print("\nNormal ordering:")
+print(QON)
+
+print("\nTransformation between Boson and Quad operators:")
+from openfermion.transforms import get_quad_operator, get_boson_operator
+O2 = get_boson_operator(QO, hbar = 2)
+print("\nTrasnforming from quadOp QO to a bosonic operator:")
+print(O2)
---
+++ II-Data Structures/D.Molecular_data_structure.py
@@ -0,0 +1,25 @@
+# This is very basic configuration for H2 in OpenFermion for it's use in
+# posterior documents
+ 
+# A. Molecule specification and input generation:
+ 
+from openfermion.hamiltonians import MolecularData
+ 
+geometry = [['H',[0,0,0]],['H',[0,0,0.74]]]
+basis = 'sto-3g'
+multiplicity = 1
+charge = 0
+h2_molecule = MolecularData(geometry, basis, multiplicity, charge)
+
+# D. Molecular data structure
+
+from openfermion.ops import InteractionOperator
+
+core_constant, one_body_integrals, two_body_integrals = (h2_molecule.get_active_space_integrals(occupied_indices=None,
+                                                                                                active_indices=None))
+# Original code takes arguments(occupied_indices,active_indices) and raises an exception for not being defined any of them, in the
+# documentation says get_active_space_integrals(occupied_indices=None, active_indices=None) to restrict a molecule to at a spatial orbital
+# level on active space
+active_space_hamiltonian = InteractionOperator( core_constant,
+                                                one_body_integrals,
+                                                two_body_integrals)
